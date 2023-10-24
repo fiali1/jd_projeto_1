422e2ae9d87cc0f120fa59a7b41fcc0850871ddc
Vinicius Lacerda Gonsalez <vinicius.gonsalez@AD.UFABC.INT.BR>
2023-10-24 20:49:30 -0300

    Final-TelaCreditos

---
+++ Assets/Scripts/scrollTexto.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollTexto : MonoBehaviour
+{
+	public float scrollSpeed = 20;
+	public float limiteLoopingY = 2500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 pos = transform.position;
+
+		Vector3 local = transform.TransformDirection(0,1,0);
+
+		pos += local * scrollSpeed * Time.deltaTime;
+		transform.position = pos;
+
+			if(pos.y >= limiteLoopingY){
+				transform.position = new Vector3(transform.position.x, -540, transform.position.z) ;
+			}
+    }
+	
+}
